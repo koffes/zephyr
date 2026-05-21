af2f7c31af1e4f54640c8eed3fe4867fc9caadd5
Kristoffer Skøien <kristoffer.skoien@nordicsemi.no>
2026-05-21 16:39:34 +0200

    hack: Working display 5340dk
    Added counter and page
    OCT-NONE
    
    Signed-off-by: Kristoffer Skøien <kristoffer.skoien@nordicsemi.no>

--- samples/bluetooth/bap_broadcast_assistant_gui/src/display.c
+++ samples/bluetooth/bap_broadcast_assistant_gui/src/display.c
@@ -18,14 +18,45 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app);
 
-static uint32_t count;
+static lv_obj_t *screen_overview;
+static lv_style_t style_common;
+
+static lv_obj_t *header_label_1;
+
+static void page_overview_draw(void)
+{
+
+	uint32_t uptime_s = (uint32_t)(k_uptime_get() / 1000);
+	char line_buf[310] = {'\0'};
+
+	sprintf(line_buf, LV_SYMBOL_BLUETOOTH "Uptime: %d", uptime_s);
+	lv_label_set_text(header_label_1, line_buf);
+}
+
+static void timer_worker(struct k_work *work)
+{
+	if (lv_scr_act() == screen_overview) {
+		page_overview_draw();
+	} else {
+		LOG_ERR("Unknown screen active.");
+	}
+
+	lv_task_handler();
+	lv_timer_handler();
+}
+
+K_WORK_DEFINE(timer_work, timer_worker);
+
+static void gui_update_timer_handler(struct k_timer *dummy)
+{
+	k_work_submit(&timer_work);
+};
+
+K_TIMER_DEFINE(gui_update_timer, gui_update_timer_handler, NULL);
 
 int display_init(void)
 {
-	char count_str[11] = {0};
 	const struct device *display_dev;
-	lv_obj_t *hello_world_label;
-	lv_obj_t *count_label;
 	int ret;
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
@@ -34,15 +65,16 @@ int display_init(void)
 		return 0;
 	}
 
-	lv_obj_clean(lv_scr_act());
+	screen_overview = lv_obj_create(NULL);
+	lv_scr_load(screen_overview);
 
-	hello_world_label = lv_label_create(lv_screen_active());
+	lv_style_init(&style_common);
+	lv_style_set_text_font(&style_common, &lv_font_montserrat_24);
 
-	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
-
-	count_label = lv_label_create(lv_screen_active());
-	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+	/* Overview page */
+	header_label_1 = lv_label_create(screen_overview);
+	lv_obj_add_style(header_label_1, &style_common, 0);
+	lv_obj_align(header_label_1, LV_ALIGN_TOP_LEFT, 0, 0);
 
 	lv_timer_handler();
 	ret = display_blanking_off(display_dev);
@@ -51,13 +83,7 @@ int display_init(void)
 		return 0;
 	}
 
-	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count / 100U);
-			lv_label_set_text(count_label, count_str);
-		}
-		lv_timer_handler();
-		++count;
-		k_sleep(K_MSEC(10));
-	}
+	k_timer_start(&gui_update_timer, K_MSEC(25), K_MSEC(25));
+
+	return 0;
 }
